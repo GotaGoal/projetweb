d9845533a6b665a574707350d818a6e56dd7a9fb
Gardien Benjamin <benjamin.gardien@viacesi.fr>
2017-04-15 18:47:30 +0200

    Carousel Patch

--- src/BDE/PlatformBundle/Entity/Carousel.php
+++ src/BDE/PlatformBundle/Entity/Carousel.php
@@ -24,10 +24,38 @@ class Carousel
     /**
      * @var string
      *
-     * @ORM\Column(name="urlphoto", type="string", length=255)
+     * @ORM\Column(name="path", type="string", length=255)
      */
-    private $urlphoto;
 
+    public $path;
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
+    }
 
     /**
      * Get id
@@ -39,28 +67,4 @@ class Carousel
         return $this->id;
     }
 
-    /**
-     * Set urlphoto
-     *
-     * @param string $urlphoto
-     *
-     * @return Carousel
-     */
-    public function setUrlphoto($urlphoto)
-    {
-        $this->urlphoto = $urlphoto;
-
-        return $this;
-    }
-
-    /**
-     * Get urlphoto
-     *
-     * @return string
-     */
-    public function getUrlphoto()
-    {
-        return $this->urlphoto;
-    }
 }
-
